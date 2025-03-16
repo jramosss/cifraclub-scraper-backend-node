d9655cec15b9f9fcb283c3374cd102f2f758eba7
jramosss <jramostod@gmail.com>
2025-03-16 16:56:11 -0300

    firebase init

--- src/pdf.ts
+++ src/pdf.ts
@@ -1,11 +1,13 @@
 import { chromium as PlaywrightChromium } from 'playwright';
 
-export async function htmlToPdf(html: string, pdfPath: string) {
+export async function htmlToPdf(html: string) {
     const browser = await PlaywrightChromium.launch({
         headless: true,
     });
     const page = await browser.newPage();
     await page.setContent(html);
-    await page.pdf({ path: pdfPath, format: 'A4', margin: { top: '10mm', right: '10mm', bottom: '10mm', left: '10mm' } });
+    const buffer = await page.pdf({ format: 'A4', margin: { top: '10mm', right: '10mm', bottom: '10mm', left: '10mm' } });
     await browser.close();
+
+    return new Blob([buffer], { type: 'application/pdf' });
 }
--- src/services.ts
+++ src/services.ts
@@ -1,3 +1,5 @@
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { storage } from "./firebase";
 import { htmlToPdf } from "./pdf";
 import { getScraper } from "./utils/utils";
 
@@ -5,13 +7,16 @@ export async function scrapeAndGenerate(connectionId: string, listUrl: string) {
 	const scraper = getScraper(connectionId);
 	const htmlContent = await scraper.scrape(listUrl);
 
-	const pdfFileName = `${connectionId}.pdf`;
-	const pdfPath = `./static/${pdfFileName}`;
+	const blob = await htmlToPdf(htmlContent);
+	const dbRef = ref(storage, `${connectionId}.pdf`);
+	const result = await uploadBytes(dbRef, blob, {
+		contentType: "application/pdf",
+	})
 
-	await htmlToPdf(htmlContent, pdfPath);
+	const url = await getDownloadURL(result.ref);
 
 	return {
-		pdfFileName,
+		url,
 		total_songs: scraper.total_songs,
 	};
 }
